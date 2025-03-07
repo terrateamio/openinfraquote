f144a661f4a2145af915f2b475918f24a584a777
Jms Dnns <jdennis@gmail.com>
2025-03-07 14:43:59 -0500

    Added tests for the match set code and updated the Makefile to run pds again if new test files are created

--- tests/oiq/test_match_set.ml
+++ tests/oiq/test_match_set.ml
@@ -1,10 +1,10 @@
 let test_basic_keys () =
   let params = [ ("name", "Jms Dnns"); ("age", "21"); ("is_drummer", "yup") ] in
-  assert (Oiq_match_set.to_keys params = [ "name=John%20Doe&age=30&city=New%20York" ])
+  assert (Oiq_match_set.to_keys params = [ "name=Jms%20Dnns"; "age=21"; "is_drummer=yup" ])
 
 let test_empty_keys () =
   let params = [] in
-  assert (Oiq_match_set.to_keys params = [ "" ])
+  assert (Oiq_match_set.to_keys params = [])
 
 let test_single_key () =
   let params = [ ("fiona", "apple") ] in
@@ -12,6 +12,7 @@ let test_single_key () =
 
 let test_special_chars () =
   let params = [ ("has some spaces", "spaces & special=chars") ] in
+  CCList.iter (fun s -> Printf.printf "%s\n" s) @@ Oiq_match_set.to_keys params;
   assert (Oiq_match_set.to_keys params = [ "has%20some%20spaces=spaces%20%26%20special%3Dchars" ])
 
 let test_subset () =
@@ -22,10 +23,20 @@ let test_subset () =
 let test_of_string () =
   assert (CCResult.is_ok @@ Oiq_match_set.of_string "resource_type=ec2&instance_type=m4.large")
 
-let test_subset_of_string () =
-  let ms1 = CCResult.get_exn @@ Oiq_match_set.of_string "foo&bar" in
-  let ms2 = CCResult.get_exn @@ Oiq_match_set.of_list [ "foo"; "fighters"; "bar" ] in
+let test_subset_for_matched_sets () =
+  let ms1 = CCResult.get_exn @@ Oiq_match_set.of_string "foo=fighters&amon=tobin" in
+  let ms2 = CCResult.get_exn @@ Oiq_match_set.of_list [ "foo=fighters"; "amon=tobin" ] in
   assert (Oiq_match_set.subset ~super:ms2 ms1)
+
+let test_subset_passes_if_superset_is_bigger () =
+  let ms1 = CCResult.get_exn @@ Oiq_match_set.of_string "foo=fighters" in
+  let ms2 = CCResult.get_exn @@ Oiq_match_set.of_list [ "foo=fighters"; "amon=tobin" ] in
+  assert (Oiq_match_set.subset ~super:ms2 ms1)
+
+let test_subset_fails_if_superset_is_smaller () =
+  let ms1 = CCResult.get_exn @@ Oiq_match_set.of_string "foo=fighters&amon=tobin" in
+  let ms2 = CCResult.get_exn @@ Oiq_match_set.of_list [ "foo=fighters" ] in
+  assert (not (Oiq_match_set.subset ~super:ms2 ms1))
 
 let () =
   test_basic_keys ();
@@ -34,4 +45,6 @@ let () =
   test_special_chars ();
   test_subset ();
   test_of_string ();
-  test_subset_of_string ()
+  test_subset_for_matched_sets ();
+  test_subset_passes_if_superset_is_bigger ();
+  test_subset_fails_if_superset_is_smaller ()
