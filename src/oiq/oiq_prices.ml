844a93b1aec57b10f9ce0b2a008c36fdcde61319
Jms Dnns <jdennis@gmail.com>
2025-03-11 13:54:15 -0400

    Updated oiq_prices.to_yojson to deserialize the price info json from the CSV so all data is one layer of json

--- src/oiq/oiq_prices.ml
+++ src/oiq/oiq_prices.ml
@@ -14,5 +14,18 @@ module Product = struct
     | _ -> raise (Failure "NYI")
 
   let to_match_set t = CCResult.get_exn @@ Oiq_match_set.of_string t.match_set_str
-  let to_yojson t = to_yojson t
+
+  (* price_info is a json string in the CSV. we parse it before printing our json *)
+  let to_yojson t =
+    let price_info_json =
+      try Yojson.Safe.from_string t.price_info
+      with Yojson.Safe.Util.Type_error (_, _) | Yojson.Json_error _ -> `String t.price_info
+    in
+    `Assoc
+      [
+        ("service", `String t.service);
+        ("product_family", `String t.product_family);
+        ("match_set_str", `String t.match_set_str);
+        ("price_info", price_info_json);
+      ]
 end
