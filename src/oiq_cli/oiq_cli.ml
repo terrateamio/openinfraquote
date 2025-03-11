96edc52e7cdde37e9b69a57d6585e1a8be22a766
Harsh Mishra <erbeusgriffincasper@gmail.com>
2025-03-11 23:34:09 +0530

    Add pricing sub-command

--- src/oiq_cli/oiq_cli.ml
+++ src/oiq_cli/oiq_cli.ml
@@ -14,20 +14,35 @@ module Cli = struct
     C.Arg.(required & opt (some dir) None & info [ "p"; "pricing-root" ] ~docv ~doc)
 
   (* -o | --output-path <filepath> *)
-  let output_path =
-    let docv = "OUTPUT_PATH" in
-    let doc = "The path for writing matched price info." in
-    C.Arg.(value & opt (some string) None & info [ "o"; "output-path" ] ~docv ~doc)
+  let output =
+    let docv = "OUTPUT_FILE" in
+    let doc = "The path for writing matched price info, stdout if not specified." in
+    C.Arg.(value & opt (some string) None & info [ "o"; "output" ] ~docv ~doc)
 
   let match_cmd f =
-    let doc = "Price matching commands" in
+    let doc = "Match resource to pricing rows." in
     let exits = C.Cmd.Exit.defaults in
     C.Cmd.v
       (C.Cmd.info "match" ~doc ~exits)
-      C.Term.(const f $ pricing_root $ resource_file $ output_path)
+      C.Term.(const f $ pricing_root $ resource_file $ output)
+
+  let usage =
+    let docv = "USAGE_FILE" in
+    let doc = "File describing usage of specific resources to be used in pricing." in
+    C.Arg.(value & opt (some file) None & info [ "u"; "usage" ] ~docv ~doc)
+
+  let input =
+    let docv = "INPUT_FILE" in
+    let doc = "Path to file generated in 'match', stdin if not specified." in
+    C.Arg.(value & opt (some file) None & info [ "i"; "input" ] ~docv ~doc)
+
+  let price_cmd f =
+    let doc = "Price a match." in
+    let exits = C.Cmd.Exit.defaults in
+    C.Cmd.v (C.Cmd.info "price" ~doc ~exits) C.Term.(const f $ usage $ input)
 end
 
-let run pricing_root resource_files output_path =
+let match_ pricing_root resource_files output_path =
   Oiq.match_
     ~pricing_root
     ~resource_files
@@ -36,6 +51,8 @@ let run pricing_root resource_files output_path =
       | Some path -> open_out path
       | None -> stdout)
 
+let price usage input = raise (Failure "nyi")
+
 let () =
   let info = Cmdliner.Cmd.info "oiq" in
-  exit @@ Cmdliner.Cmd.eval @@ Cmdliner.Cmd.group info [ Cli.match_cmd run ]
+  exit @@ Cmdliner.Cmd.eval @@ Cmdliner.Cmd.group info [ Cli.match_cmd match_; Cli.price_cmd price ]
