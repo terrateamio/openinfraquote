c9b2b90282c23b9ee39ae02d4e5790bb162d64b4
Malcolm <malcolm@terrateam.io>
2025-03-19 12:00:43 +0100

    REFACTOR Make error handling a bit more structured

--- src/oiq/oiq_prices.mli
+++ src/oiq/oiq_prices.mli
@@ -3,9 +3,18 @@ module Price : sig
 end
 
 module Product : sig
+  type of_row_err =
+    [ `Invalid_price_json_err of string
+    | `Invalid_row_err of string list
+    | `Invalid_usd_err of Yojson.Safe.t
+    | `Invalid_unit_err of Yojson.Safe.t
+    | `Invalid_price_err of Yojson.Safe.t * string
+    ]
+  [@@deriving show]
+
   type t
 
-  val of_row : string list -> t
+  val of_row : string list -> (t, [> of_row_err ]) result
   val to_match_set : t -> Oiq_match_set.t
   val to_yojson : t -> Yojson.Safe.t
 end
